627f21af163297f40b95d2cf75abd35747fc88e2
Fernando Mazola <fernando_mazola@hotmail.com>
2017-03-02 11:07:19 -0300

    Relembrando ArrayAdapter, Componente AutoComplete, TimePicker, DatePicker, Tipos de Dialogs

---
+++ app/src/main/java/pickeranddialogs/fiap/com/br/pickeranddialogs/Dialogs.java
@@ -0,0 +1,15 @@
+package pickeranddialogs.fiap.com.br.pickeranddialogs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Dialogs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dialogs);
+
+
+    }
+}
