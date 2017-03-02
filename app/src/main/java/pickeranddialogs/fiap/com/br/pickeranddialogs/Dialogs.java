f54753e8c846d15c2afbdee4c986596afa4e7fcf
Fernando Mazola <fernando_mazola@hotmail.com>
2017-03-02 11:35:15 -0300

    Adicionado dialog progress com parametros

--- app/src/main/java/pickeranddialogs/fiap/com/br/pickeranddialogs/Dialogs.java
+++ app/src/main/java/pickeranddialogs/fiap/com/br/pickeranddialogs/Dialogs.java
@@ -1,10 +1,12 @@
 package pickeranddialogs.fiap.com.br.pickeranddialogs;
 
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 public class Dialogs extends AppCompatActivity {
@@ -17,24 +19,32 @@ public class Dialogs extends AppCompatActivity {
 
     }
 
-    public void salvar(View v){
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle(R.string.titulo);
-        builder.setMessage(R.string.msg);
-        builder.setCancelable(false);
-        //builder.setNeutralButton(R.string.fechar, null);
-        builder.setNegativeButton(R.string.fechar, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                Toast.makeText(Dialogs.this, getString(R.string.clicou_em_fechar), Toast.LENGTH_SHORT).show();
-            }
-        });
-        builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                Toast.makeText(Dialogs.this, getString(R.string.clicou_em_ok), Toast.LENGTH_SHORT).show();
-            }
-        });
-        builder.show();
+//    public void salvar(View v){
+//        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+//        builder.setTitle(R.string.titulo);
+//        builder.setMessage(R.string.msg);
+//        builder.setCancelable(false);
+//        builder.setIcon(android.R.drawable.ic_dialog_alert);
+//        //builder.setNeutralButton(R.string.fechar, null);
+//        builder.setNegativeButton(R.string.fechar, new DialogInterface.OnClickListener() {
+//            @Override
+//            public void onClick(DialogInterface dialog, int which) {
+//                Toast.makeText(Dialogs.this, getString(R.string.clicou_em_fechar), Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//        builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+//            @Override
+//            public void onClick(DialogInterface dialog, int which) {
+//                Toast.makeText(Dialogs.this, getString(R.string.clicou_em_ok), Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//        builder.show();
+//    }
+
+    public void salvar1 (View v){
+        ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Cagrrenando");
+        progressDialog.setMessage("Aguarde...");
+        progressDialog.show();
     }
 }
